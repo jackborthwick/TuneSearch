b0f4d47392aa9082453aead37385af65522d4b82
Jack Borthwick <jackmckeanborthwick@gmail.com>
2015-06-15 12:42:04 -0400

    Initial Commit

---
+++ TuneSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TuneSearch
+//
+//  Created by Jack Borthwick on 6/15/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TuneSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TuneSearch
+//
+//  Created by Jack Borthwick on 6/15/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
